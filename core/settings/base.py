bd7f5a4cddad7a9b2b6d1e01d386e0232b58f879
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-03-21 18:56:08 -0500

    feat: configure authentication URLs and redirects

--- core/settings/base.py
+++ core/settings/base.py
@@ -83,3 +83,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
