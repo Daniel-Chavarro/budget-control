3030c410dd695fa266bb824d40d7a81019d7fb7b
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-03-21 19:56:37 -0500

    feat: add pending receipts count to navigation

--- core/settings/base.py
+++ core/settings/base.py
@@ -44,6 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'budget.context_processors.pending_receipts_count',
             ],
         },
     },
