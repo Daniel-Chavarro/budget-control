3a9e76373b7ecb1976a25897396c649930ec5e93
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-03-24 08:20:51 -0500

    chore: remove unit model files

--- budget/models/__init__.py
+++ budget/models/__init__.py
@@ -1,6 +1,5 @@
 """Budget app models."""
 from .user import UserProfile
-from .unit import Unit, UserUnit
 from .receipt import Receipt, Category
 
-__all__ = ['UserProfile', 'Unit', 'UserUnit', 'Receipt', 'Category']
+__all__ = ['UserProfile', 'Receipt', 'Category']
